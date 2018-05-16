cd573aacc6311c5b77cbd6ab47c61316aebfc901
Markus Rempfler <markus.rempfler@fmi.ch>
2018-05-16 10:44:14 +0200

    Amend dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,26 @@ from glob import glob
 import os
 from setuptools import setup, find_packages
 
-
-contrib = ['Markus Rempfler',]
+contrib = [
+    'Markus Rempfler',
+]
 
 # setup.
-setup(name='dl-utils',
-      version='0.1',
-      description='deep learning utilities',
-      author=', '.join(contrib),
-      license='BSD',
-      packages=find_packages(exclude=['tests', ]),
-      install_requires=['numpy',
-                        'matplotlib',
-                        'scipy',
-                        'keras',],
-      # I believe zipping makes the import rather slow.
-      zip_safe=False
-      )
+setup(
+    name='dl-utils',
+    version='0.1',
+    description='deep learning utilities',
+    author=', '.join(contrib),
+    license='BSD',
+    packages=find_packages(exclude=[
+        'tests',
+    ]),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'keras==2.1.6',
+        'opencv-python>=3.4'
+    ],
+    # I believe zipping makes the import rather slow.
+    zip_safe=False)
