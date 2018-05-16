a126e6870aa63545bee19cd3934b9c15f8ffd3f3
Markus Rempfler <markus.rempfler@fmi.ch>
2018-05-16 08:40:55 +0200

    Add a first version of intensity augemtnations

--- dlutils/training/sampling.py
+++ dlutils/training/sampling.py
@@ -22,7 +22,9 @@ def get_random_patch_corner(img_shape, patch_size):
 def get_random_patch(channels,
                      patch_size,
                      horizontal_flip=False,
-                     vertical_flip=False):
+                     vertical_flip=False,
+                     intensity_scale=None,
+                     intensity_shift=None):
     '''sample a random patch from all given channels.
 
     channels : list of 2D images
@@ -46,5 +48,14 @@ def get_random_patch(channels,
         if np.random.random() < 0.5:
             for idx, patch in enumerate(patches):
                 patches[idx] = flip_axis(patch, 0)
+
+    dtype = patches[0].dtype
+    if intensity_scale is not None:
+        scale = 1 + (np.random.randn(1) * intensity_scale)
+        patches[0] = (scale * patches[0]).astype(dtype)
+
+    if intensity_shift is not None:
+        shift = np.random.randn(1) * intensity_shift
+        patches[0] = patches[0] + (patches[0].mean() * shift).astype(dtype)
 
     return patches
