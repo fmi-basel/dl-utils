daa0ce85aa2c52b139c0bffca3f3d28b9e2bbc1c
Markus Rempfler <markus.rempfler@fmi.ch>
2018-05-18 17:01:47 +0200

    Fix issue with cos scheduler and keras callback

--- dlutils/training/scheduler.py
+++ dlutils/training/scheduler.py
@@ -1,17 +1,13 @@
 import numpy as np
 
 
-class CosineAnnealingSchedule(object):
-    def __init__(self, lr_max, lr_min, epoch_max):
-        '''
-        '''
-        self.lr_min = lr_min
-        self.lr_max = lr_max
-        self.epoch_max = epoch_max
+def CosineAnnealingSchedule(lr_max, lr_min, epoch_max):
 
-    def schedule(self, epoch, current_lr):
+    def schedule(epoch, current_lr):
         '''
         '''
         cosine_factor = (
-            1 + np.cos(float(epoch % self.epoch_max) / self.epoch_max * np.pi))
-        return self.lr_min + 0.5 * (self.lr_max - self.lr_min) * cosine_factor
+            1 + np.cos(float(epoch % epoch_max) / epoch_max * np.pi))
+        return lr_min + 0.5 * (lr_max - lr_min) * cosine_factor
+
+    return schedule
