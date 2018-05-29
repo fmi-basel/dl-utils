46db083a31e1b9c14a020d807c0923e00e3e982f
Markus Rempfler <markus.rempfler@fmi.ch>
2018-05-29 10:41:38 +0200

    Add whitening standardization

--- dlutils/preprocessing/normalization.py
+++ dlutils/preprocessing/normalization.py
@@ -12,3 +12,11 @@ def normalize(img, offset=0, scale=1, min_std=0.):
 
     mean = img.mean()
     return (np.tanh((img - mean) / std)) * scale + offset
+
+
+def standardize(img, min_scale=0.):
+    '''normalize intensities according to whitening transform.
+    '''
+    mean = img.mean()
+    scale = max(min_scale, img.std())
+    return (img - mean) / float(scale)
