54571031c811660a931b9c35b842b29de4b41253
Markus Rempfler <markus.rempfler@fmi.ch>
2018-10-19 14:47:48 +0200

    Add task-id utility

--- dlutils/utils.py
+++ dlutils/utils.py
@@ -6,3 +6,17 @@ def set_seeds(seed_val=42):
     seed(seed_val)
     from tensorflow import set_random_seed
     set_random_seed(seed_val)
+
+
+def get_zero_based_task_id():
+    '''fetches the environment variable for this process' task id.
+
+    Returns None if process is not run in an SGE environment.
+
+    '''
+    import os
+    sge_id = os.environ.get('SGE_TASK_ID', None)
+    if sge_id is None:
+        return None
+
+    return int(sge_id) - 1
