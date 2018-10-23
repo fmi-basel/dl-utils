fbce34fece21d207d87a8f592e8a18f1d7d5e1f6
Markus Rempfler <markus.rempfler@fmi.ch>
2018-10-23 08:23:11 +0200

    Add option to set default value in get_task_id for non-sge environments

--- dlutils/utils.py
+++ dlutils/utils.py
@@ -8,7 +8,7 @@ def set_seeds(seed_val=42):
     set_random_seed(seed_val)
 
 
-def get_zero_based_task_id():
+def get_zero_based_task_id(default_return=None):
     '''fetches the environment variable for this process' task id.
 
     Returns None if process is not run in an SGE environment.
@@ -17,6 +17,6 @@ def get_zero_based_task_id():
     import os
     sge_id = os.environ.get('SGE_TASK_ID', None)
     if sge_id is None:
-        return None
+        return default_return
 
     return int(sge_id) - 1
