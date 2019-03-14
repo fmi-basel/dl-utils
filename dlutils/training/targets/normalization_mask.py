0cbd2bbea7771788df102fbe1819274e3c9d82ba
rfortiz <raphael.ortiz@fmi.ch>
2019-03-14 10:12:53 +0100

    modified normalization mask to ignore negative labels

--- dlutils/training/targets/normalization_mask.py
+++ dlutils/training/targets/normalization_mask.py
@@ -3,15 +3,14 @@ import numpy as np
 def generate_normalization_mask(segmentation, include_background=False):
     ''' Computes an instance wise weighting mask.
     
-    Notes:
-    ------
-    Background expected to be labeled as 0
+    Background expected to be labeled as 0. Mask is zero where labels 
+    are negative.
     '''
 
     normalization = np.zeros_like(segmentation, dtype=np.float32)
     n_labels = 0
     for label in np.unique(segmentation):
-        if include_background or label > 0:
+        if label > 0 or (include_background and label==0):
             instance_mask = segmentation==label
             area = instance_mask.sum()    
             normalization[instance_mask] = 1./area
