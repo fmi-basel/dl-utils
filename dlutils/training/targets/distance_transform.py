11a1757a8d65fc4774f6123e5e9b2577fa39b2eb
rfortiz <raphael.ortiz@fmi.ch>
2019-05-07 10:50:13 +0200

    added boundary from annotations, relative to instance size

--- dlutils/training/targets/distance_transform.py
+++ dlutils/training/targets/distance_transform.py
@@ -39,6 +39,37 @@ def shrink_labels(segmentation, sampling=1.0, distance_thresh=0.5, val=0):
     '''
     shrunk_segmentation = segmentation.copy()
     dist = generate_distance_transform(segmentation, sampling=sampling)
-    shrunk_segmentation[dist < distance_thresh] = val
+    shrunk_segmentation[ (dist<distance_thresh) & (segmentation!=0)] = val
 
     return shrunk_segmentation
+
+
+def label_boundaries(segmentation, sampling=1.0, inner_expansion=0.2, outer_expansion=0.2, val=-1):
+    '''Define boundaries relative to each instance size
+    '''
+    
+    sampling = np.broadcast_to(np.asarray(sampling), segmentation.ndim)
+    boundary_segmentation = segmentation.copy()
+    
+    for label in filter(None, np.unique(segmentation)):
+        loc = find_objects(segmentation == label)[0]
+        sizes = [s.stop-s.start for s in loc]
+        
+        # expand the roi to surrounding region
+        loc = tuple(slice(max(0,int(sl.start-si*outer_expansion)), sl.stop+int(si*outer_expansion)) for sl, si in zip(loc,sizes))
+        
+        # instance
+        mask = segmentation[loc] == label
+        dist_transform = distance_transform_edt(mask, sampling=sampling)
+        dist_threshold_inner = dist_transform.max()*inner_expansion
+        dist_threshold_outer = dist_transform.max()*outer_expansion
+        boundary_segmentation[loc][(dist_transform<dist_threshold_inner) & mask] = val
+        
+        # background (with threshold relative to isntance size)
+        mask = np.invert(mask)
+        dist_transform = distance_transform_edt(mask, sampling=sampling)
+        boundary_segmentation[loc][(dist_transform<dist_threshold_outer) & mask] = val
+        
+    return boundary_segmentation
+        
+        
