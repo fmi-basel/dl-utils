990abea4e6334e559987983d6db7cf37c9264e87
rfortiz <raphael.ortiz@fmi.ch>
2019-05-23 12:52:28 +0200

    added min shape option to cropping fct

--- dlutils/preprocessing/crop.py
+++ dlutils/preprocessing/crop.py
@@ -1,17 +1,26 @@
 from scipy.ndimage import find_objects
+from math import ceil
+import numpy as np
 
 
-def crop_object(images, labels, margins=None):
+def crop_object(images, labels, margins=0, min_shape=0):
     '''
     Crop all images based on object found in labels.
 
     To crop labels, pass it in the images list as well.
     '''
 
-    if margins is None:
-        margins = (0,) * labels.ndim
+    margins = np.broadcast_to(np.asarray(margins), labels.ndim).copy()
+    min_shape = np.broadcast_to(np.asarray(min_shape), labels.ndim)
+
     loc = find_objects(labels >= 1)[0]
-    loc = tuple(slice(max(sli.start - margin, 0), sli.stop + margin)
-                for sli, margin in zip(loc, margins))
+
+    # if needed, increase margins to output object at least min_shape
+    object_shape = np.asarray(tuple(sli.stop - sli.start for sli in loc))
+    margins = np.maximum(margins, np.ceil((min_shape - object_shape) / 2))
+
+    loc = tuple(
+        slice(max(sli.start - margin, 0), sli.stop + margin)
+        for sli, margin in zip(loc, margins))
 
     return [image[loc] for image in images]
