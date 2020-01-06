576d891503376647c8afaffee79566e54ae8591a
rfortiz <raphael.ortiz@fmi.ch>
2020-01-06 12:23:47 +0100

    reimplemented weighted losses with shared base class

--- dlutils/losses/weighted_losses.py
+++ dlutils/losses/weighted_losses.py
@@ -1,82 +1,58 @@
 from tensorflow.keras import backend as K
 import tensorflow as tf
-import numpy as np
+import abc
 
 
-def weighted_l1_loss():
-    '''Computes L1 loss, using the last channel of y_true as 
-    normalization weights
-
-    Notes:
-    ------
-    - sum of weights == 1 is expected
-    - implemented as channels_last
-
-    '''
-    def loss(y_true, y_pred):
+class PixelWeightedLossBase(tf.keras.losses.Loss):
+    def call(self, y_true, y_pred):
+        '''Computes pixel weighted loss, using the last channel of y_true as 
+        normalization weights
+    
+        Notes:
+        ------
+        - sum of weights == 1 is expected
+        - implemented as channels_last
         '''
-        '''
+
         # extract pre-computed normalization channel
+        self._check_weights_stacking(y_true, y_pred)
         weights = y_true[..., -1:]
         y_true = y_true[..., 0:-1]
 
-        loss = K.abs(y_pred - y_true)
-        # sum over spatial dims, mean over batch and channel dims
+        loss = self._pixel_loss(y_true, y_pred)
+
+        # sum loss over spatial and channel dims, mean over batch
         loss = tf.math.reduce_sum(loss * weights,
                                   axis=tuple(range(1, len(y_pred.shape))))
         return tf.math.reduce_mean(loss)
 
-    return loss
+    def _check_weights_stacking(self, y_true, y_pred):
+        if y_true.shape[-1] != y_pred.shape[-1] + 1:
+            raise ValueError(
+                'Weights incorrectly stacked. Expected y_true with an extra channel for the weights got y_true, y_pred shapes: {} and {}'
+                .format(y_true.shape, y_pred.shape))
+
+    @abc.abstractmethod
+    def _pixel_loss(self, y_true, y_pred):
+        pass
 
 
-def weighted_l2_loss():
-    '''Computes L1 loss, using the last channel of y_true as 
-    normalization weights
-
-    Notes:
-    ------
-    - sum of weights == 1 is expected
-    - implemented as channels_last
-
-    '''
-    def loss(y_true, y_pred):
-        '''
-        '''
-        # extract pre-computed normalization channel
-        weights = y_true[..., -1:]
-        y_true = y_true[..., 0:-1]
-
-        loss = K.square(y_pred - y_true)
-        # sum over spatial dims, mean over batch and channel dims
-        loss = tf.math.reduce_sum(loss * weights,
-                                  axis=tuple(range(1, len(y_pred.shape))))
-        return tf.math.reduce_mean(loss)
-
-    return loss
+class PixelWeightedL1Loss(PixelWeightedLossBase):
+    def _pixel_loss(self, y_true, y_pred):
+        return K.abs(y_pred - y_true)
 
 
-def weighted_binary_crossentropy(from_logits=False):
-    '''Computes binary cross-entropy loss, using the last channel of 
-    y_true  as normalization weights
+class PixelWeightedMSE(PixelWeightedLossBase):
+    def _pixel_loss(self, y_true, y_pred):
+        return K.square(y_pred - y_true)
 
-    Notes:
-    ------
-    - sum of weights == 1 is expected
-    - implemented as channels_last
 
-    '''
-    def loss(y_true, y_pred):
-        '''
-        '''
+class PixelWeightedBinaryCrossentropy(PixelWeightedLossBase):
+    def __init__(self, from_logits=False):
+        super().__init__()
+        self.from_logits = from_logits
 
-        # extract pre-computed normalization channel
-        weights = y_true[..., -1:]
-        y_true = y_true[..., 0:-1]
-
-        loss = K.binary_crossentropy(y_true, y_pred, from_logits=from_logits)
-        # sum over spatial dims, mean over batch and channel dims
-        loss = tf.math.reduce_sum(loss * weights,
-                                  axis=tuple(range(1, len(y_pred.shape))))
-        return tf.math.reduce_mean(loss)
-
-    return loss
+    def _pixel_loss(self, y_true, y_pred):
+        return K.binary_crossentropy(y_true,
+                                     y_pred,
+                                     from_logits=self.from_logits)
