2fee8698784f3a4b3d9b8c9d19d5def6bcdbcfae
rfortiz <raphael.ortiz@fmi.ch>
2020-03-25 11:12:26 +0100

    add fg masking

--- dlutils/losses/jaccard_loss.py
+++ dlutils/losses/jaccard_loss.py
@@ -1,5 +1,50 @@
 import tensorflow as tf
 
+# TODO separate multi-class and binary cases (needed to handle unannotated labels)
+
+
+class BinaryJaccardLoss(tf.keras.losses.Loss):
+    '''
+    Differentiable Jaccard/mIoU loss as proposed in:
+    
+    Rahman, Md Atiqur, and Yang Wang. "Optimizing intersection-over-union 
+    in deep neural networks for image segmentation." 
+    International symposium on visual computing. Springer, Cham, 2016.
+    
+    if y_true has 2 channels, the second channel is considered annotation mask
+    '''
+    def __init__(self, eps=1e-6, clip_probs=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.eps = eps
+        self.clip_probs = clip_probs
+
+    def call(self, y_true, y_pred):
+        '''
+        Args:
+        
+        y_true: 1 hot masks
+        y_pred: probability maps between [0,1] for each label
+        '''
+
+        if y_true.shape[-1] > 1:
+            fg_mask = tf.cast(y_true[..., 1:2], tf.float32)
+            y_true = y_true[..., 0:1]
+
+            y_pred = y_pred * fg_mask
+
+        if self.clip_probs is not None:
+            y_pred = tf.clip_by_value(y_pred, *self.clip_probs)
+
+        spatial_axis = tuple(range(1, len(y_true.shape) - 1))
+
+        intersection = tf.reduce_sum(y_pred * y_true, axis=spatial_axis)
+        union = tf.reduce_sum(
+            (y_pred + y_true), axis=spatial_axis) - intersection
+        jaccard = 1. - (intersection + self.eps) / (union + self.eps)
+
+        return tf.math.reduce_mean(jaccard)
+
 
 class JaccardLoss(tf.keras.losses.Loss):
     '''
@@ -9,10 +54,17 @@ class JaccardLoss(tf.keras.losses.Loss):
     in deep neural networks for image segmentation." 
     International symposium on visual computing. Springer, Cham, 2016.
     '''
-    def __init__(self, eps=1e-6, *args, **kwargs):
+    def __init__(self,
+                 eps=1e-6,
+                 clip_probs=None,
+                 fg_only=False,
+                 *args,
+                 **kwargs):
         super().__init__(*args, **kwargs)
 
         self.eps = eps
+        self.clip_probs = clip_probs
+        self.fg_only = fg_only
 
     def call(self, y_true, y_pred):
         '''
@@ -21,6 +73,22 @@ class JaccardLoss(tf.keras.losses.Loss):
         y_true: 1 hot masks
         y_pred: probability maps between [0,1] for each label
         '''
+
+        if self.fg_only:
+            fg_mask = tf.cast(
+                tf.reduce_any(tf.greater_equal(y_true, 0.5),
+                              axis=-1,
+                              keepdims=True), tf.float32)
+
+            y_pred = y_pred * fg_mask
+
+        if self.clip_probs is not None:
+            # ~y_pred = tf.clip_by_value(y_pred, *self.clip_probs)
+
+            # clip where overconfident
+            y_pred = tf.where(tf.cast(y_true, tf.bool),
+                              tf.minimum(y_pred, self.clip_probs[1]),
+                              tf.maximum(y_pred, self.clip_probs[0]))
 
         spatial_axis = tuple(range(1, len(y_true.shape) - 1))
 
--- tests/test_jaccard_loss.py
+++ tests/test_jaccard_loss.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from dlutils.losses.jaccard_loss import JaccardLoss
 
+# TODO update once binary and multiclass are treated separately
+
 
 def test_JaccardLoss():
     '''Verifies that the soft Jaccard loss behaves as keras MeanIoU when 
@@ -28,6 +30,35 @@ def test_JaccardLoss():
 
     perfect_loss = JaccardLoss(eps=0)(one_hot, one_hot)
     np.testing.assert_almost_equal(perfect_loss, 0., decimal=3)
+
+
+def test_JaccardLoss_fgonly():
+    '''Verifies that the soft Jaccard loss behaves as keras MeanIoU when 
+    probabilities are either 0 or 1 AND annotations are partial,
+    contains unannot label < 0.
+    '''
+    np.random.seed(25)
+    n_classes = 4
+
+    # random labels, 4 classes + unannot label = -1, batch size = 4
+    yt = np.random.choice(range(-1, n_classes), size=(4, 10, 10, 1))
+    yp = np.random.choice(range(0, n_classes), size=(4, 10, 10, 1))
+    labelled_mask = yt >= 0
+
+    m = tf.keras.metrics.MeanIoU(num_classes=n_classes)
+    m.update_state(yt[labelled_mask], tf.cast(yp[labelled_mask], tf.int32))
+    expected_loss = 1. - m.result().numpy()
+
+    one_hot = tf.cast(tf.one_hot(tf.squeeze(yt, -1), n_classes), tf.float32)
+    probs = tf.cast(tf.one_hot(tf.squeeze(yp, -1), n_classes), tf.float32)
+    loss = JaccardLoss(eps=0, fg_only=False)(one_hot, probs)
+
+    loss_fg = JaccardLoss(eps=0, fg_only=True)(one_hot, probs)
+
+    np.testing.assert_almost_equal(loss, expected_loss, decimal=3)
+
+    # ~perfect_loss = JaccardLoss(eps=0)(one_hot, one_hot)
+    # ~np.testing.assert_almost_equal(perfect_loss, 0., decimal=3)
 
 
 def test_JaccardLoss_training():
