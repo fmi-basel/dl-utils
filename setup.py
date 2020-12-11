3fcc92ab59bf929bf993025ffe53160bb0368c82
rfortiz <raphael.ortiz@fmi.ch>
2020-12-11 12:34:05 +0100

    bump tf and tfa versions to be compatible and support additive semi-conv

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     ]),
     install_requires=[
         'numpy', 'matplotlib', 'scipy', 'opencv-python>=3.4',
-        'tensorflow>=2.0,<2.2', 'scikit-image>=0.13', 'scikit-learn', 'future',
-        'tqdm', 'pytest', 'pyyaml', 'h5py', 'tensorflow-addons>=0.6.0,<=0.8.3'
+        'tensorflow>=2.3', 'scikit-image>=0.13', 'scikit-learn', 'future',
+        'tqdm', 'pytest', 'pyyaml', 'h5py', 'tensorflow-addons>=0.11.2'
     ],
     # I believe zipping makes the import rather slow.
     zip_safe=False)
