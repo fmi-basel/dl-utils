98c9c43a63229b66291755203489f5399423c38b
Markus Rempfler <markus.rempfler@fmi.ch>
2021-02-12 09:30:51 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -6,19 +6,17 @@ contrib = [
 ]
 
 # setup.
-setup(
-    name='dl-utils',
-    version='0.1',
-    description='deep learning utilities',
-    author=', '.join(contrib),
-    license='BSD',
-    packages=find_packages(exclude=[
-        'tests',
-    ]),
-    install_requires=[
-        'numpy', 'scipy', 'tensorflow>=2.3,<2.4', 'scikit-image>=0.13',
-        'scikit-learn', 'future', 'tqdm', 'pytest', 'pyyaml', 'h5py',
-        'tensorflow-addons==0.11.2'
-    ],
-    # I believe zipping makes the import rather slow.
-    zip_safe=False)
+setup(name='dl-utils',
+      version='0.2.0',
+      description='deep learning utilities',
+      author=', '.join(contrib),
+      license='MIT',
+      packages=find_packages(exclude=[
+          'tests',
+      ]),
+      install_requires=[
+          'numpy', 'scipy', 'tensorflow>=2.3,<2.4', 'scikit-image>=0.13',
+          'scikit-learn', 'future', 'tqdm', 'pytest', 'pyyaml', 'h5py',
+          'tensorflow-addons==0.11.2'
+      ],
+      zip_safe=False)
